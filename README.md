57f748e079189953f0f8ff8fe46e591f25ef0c5f
José Coelho <camilocoelho93@gmail.com>
2019-07-13 19:36:39 +0100

    add features

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 ## Node.js CRUD API
 
 This project is inspired on the php-crud-api
+
+## Features
+
+The following features are supported:
+
+* [X] Single file, easy to deploy.
+* [X] Very little code, easy to adapt and maintain
+* [ ] Supports POST variables as input (x-www-form-urlencoded)
+* [X] Supports a JSON object as input
+* [ ] Supports a JSON array as input (batch insert)
+* [ ] Sanitize and validate input using callbacks
+* [ ] Permission system for databases, tables, columns and records
+* [ ] Multi-tenant database layouts are supported
+* [ ] Multi-domain CORS support for cross-domain requests
+* [ ] Support for reading joined results from multiple tables
+* [ ] Search support on multiple criteria
+* [ ] Pagination, sorting, top N list and column selection
+* [ ] Relation detection with nested results (belongsTo, hasMany and HABTM)
+* [ ] Atomic increment support via PATCH (for counters)
+* [ ] Binary fields supported with base64 encoding
+* [ ] Spatial/GIS fields and filters supported with WKT and GeoJSON
+* [ ] Generate API documentation using OpenAPI tools
+* [ ] Authentication via JWT token or username/password
+* [ ] Support for reading database structure in JSON
+* [ ] Support for modifying database structure using REST endpoint
+* [ ] Security enhancing middleware is included
+* [ ] Standard compliant: PSR-2, PSR-4, PSR-7, PSR-15 and PSR-17
--- index.js
+++ index.js
@@ -5,14 +5,7 @@ var config = {
         user: 'grafana_user',
         password: 'grafana',
         database: 'weather',
-    },
-    mappings:[
-        {
-            field: 'scmInfo.scmCity',
-            table: 'temperatures',
-            column: 'city'
-        }
-    ]
+    }
 }
 
 var express = require('express');
@@ -22,68 +15,73 @@ var app = express();
 app.use(express.json())
 
 var con = mysql.createConnection(config.mysql);
+con.connect(function(err){
+    if(err) console.log(err);
+});
+
+app.delete('/records/:table/:id', function(req, res){
+    var body = req.body;
+
+    sql = "DELETE FROM " + req.params.table + " WHERE id = " + req.params.id
+
+    con.query(sql, values, function(err,result){
+        if(err) res.send(err);
+        console.log(sql);
+        res.send(result);
+    });
+});
+
+app.put('/records/:table/:id', function(req, res){
+    var body = req.body;
+    var columns = Object.keys(body);
+    var values = Object.values(body);
+
+    sql = "UPDATE " + req.params.table + " SET "
+    columns.forEach(function(column, index){
+        sql += column + " = ?"
+        if(index != columns.length-1)
+            sql += ", "
+    });
+
+    con.query(sql, values, function(err,result){
+        if(err) res.send(err);
+        console.log(sql);
+        res.send(result);
+    });
+});
 
 app.post('/records/:table', function(req, res){
     var body = req.body;
     var table = req.params.table
 
-    config.mappings.forEach(function(elem){
-        if(elem.table == table){
-            console.log("111")
-            body[elem.column] = resolveProperties(body, elem.field);
-            delete body[elem.field.split(".")[0]];
-        }
-    });
-
     var columns = Object.keys(body);
     var values = Object.values(body);
     
-    sql = "INSERT INTO " + req.params.table + " (" + columns.join() + ") VALUES ("
-    values = values.map(elem => (typeof elem == 'string' ? "'" + elem + "'" : elem))
-    sql += values.join() + ")"
+    sql = "INSERT INTO " + table + " (" + columns.join() + ") VALUES (?" + ",?".repeat(values.length - 1) + ")"
 
-    con.connect(function(err){
-        con.query(sql, function(err, result){
-            if(err) res.send(err).sendStatus('500')
-            console.log("Could not connect to MySQL");
-            res.send(result)
-        })
+    con.query(sql, values, function(err, result){
+        if(err) res.send(err);
+        res.send(result);
     })
 });
 
 app.get('/records/:table', function(req, res){
     sql = "SELECT * FROM " + req.params.table
-    con.connect(function(err){
-        con.query(sql, function(err, result){
-            if(err) res.send(err).sendStatus('500')
-            console.log("Could not connect to MySQL");
-            res.send(result)
-        })
+    con.query(sql, function(err, result){
+        if(err) res.send(err)
+        console.log("Could not connect to MySQL");
+        res.send(result)
     })
 });
 
 app.get('/records/:table/:id', function(req, res){
     sql = "SELECT * FROM " + req.params.table + " WHERE id = " + req.params.id
-    con.connect(function(err){
-        con.query(sql, function(err, result){
-            if(err) res.send(err).sendStatus('500')
-            console.log("Could not connect to MySQL");
-            res.send(result)
-        })
+    con.query(sql, function(err, result){
+        if(err) res.send(err)
+        console.log("Could not connect to MySQL");
+        res.send(result)
     })
 });
-
-function checkNested(obj, level,  ...rest) {
-    if (obj === undefined) return false
-    if (rest.length == 0 && obj.hasOwnProperty(level)) return true
-    return checkNested(obj[level], ...rest)
-}
-
-function resolveProperties(obj, path){
-    return path.split('.').reduce(function(prev, curr) {
-        return prev ? prev[curr] : null
-    }, obj || self)
-}
 
 var server = app.listen(8081, function(){
     var host = server.address().address
